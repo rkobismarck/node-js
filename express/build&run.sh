66aca0867984d0dea40f1e75b371382d3ae74d1c
Roberto Carlos Trujillo Torres <robertotrujillo@MacBook-Pro-de-Roberto.local>
2018-11-16 20:01:33 -0600

    Added support for automatic build and run in a dev environment

---
+++ express/build&run.sh
@@ -0,0 +1,13 @@
+echo "We'll build our latest SW version."
+
+docker build -t app-express .
+
+echo "Build finished, sucesfully."
+
+echo "We'll run our latest image:"
+
+docker stop $(docker ps -q --filter ancestor=app-express )
+
+docker run -d -p 8080:8080 app-express
+
+echo "We're running the latest version."
